e5ef08984202ba884840a7a0d7a093c387db6c6a
AmirHossien Karimi <amirhkz1384@gmail.com>
2026-02-21 16:26:08 +0330

    create the services host and bootstrpp the it

---
+++ Lampshade/ServiceHost/Program.cs
@@ -0,0 +1,37 @@
+using ShopManagement.Infrastructure.Configuration;
+
+namespace ServiceHost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            ShopManagementBootstrapper.Configure(builder.Services, builder.Configuration.GetConnectionString("LampShadeDB"));
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
---
+++ Lampshade/ShopManagment.Infrastructure.Configuration/ShopManagementBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ShopManagement.Application;
+using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagementDomain.ProductCategoryAgg;
+using SM.Infrastructure.EFCore;
+using SM.Infrastructure.EFCore.Repositories;
+
+namespace ShopManagement.Infrastructure.Configuration;
+
+public class ShopManagementBootstrapper
+{
+    public static void Configure(IServiceCollection services, string? connectionString)
+    {
+        services.AddTransient<IProductCategoryRepository, ProductCategoryRepository>();
+        services.AddTransient<IProductCategoryApplication, ProductCategoryApplication>();
+
+        services.AddDbContext<ShopContext>(x => x.UseSqlServer(connectionString));
+
+    }
+}
